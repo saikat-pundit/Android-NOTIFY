fb343855d16dd63e171effc8d163a5d643c2e55c
saikat-pundit <saikatpundit@yandex.com>
2026-03-03 09:24:20 +0530

    Update RestartReceiver.kt

--- app/src/main/java/com/example/notiflogger/RestartReceiver.kt
+++ app/src/main/java/com/example/notiflogger/RestartReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.PowerManager
 import android.service.notification.NotificationListenerService
 import android.util.Log
