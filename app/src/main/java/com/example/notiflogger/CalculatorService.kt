e87a26e09479d0be5400257bd42da08727f66cf3
saikat-pundit <saikatpundit@yandex.com>
2026-04-10 11:22:40 +0530

    Create CalculatorService.kt

---
+++ app/src/main/java/com/example/notiflogger/CalculatorService.kt
@@ -0,0 +1,72 @@
+package com.android.mycalculator
+
+object CalculatorService {
+    
+    fun evaluate(expression: String): Double {
+        // Standardize the string for parsing
+        val str = expression.replace("×", "*").replace("÷", "/")
+        
+        return object : Any() {
+            var pos = -1
+            var ch = 0
+
+            fun nextChar() {
+                ch = if (++pos < str.length) str[pos].code else -1
+            }
+
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.code) nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.code)) x += parseTerm() // addition
+                    else if (eat('-'.code)) x -= parseTerm() // subtraction
+                    else return x
+                }
+            }
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.code)) x *= parseFactor() // multiplication
+                    else if (eat('/'.code)) x /= parseFactor() // division
+                    else return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+'.code)) return parseFactor() // unary plus
+                if (eat('-'.code)) return -parseFactor() // unary minus
+                var x: Double
+                val startPos = pos
+                if (eat('('.code)) { // parentheses
+                    x = parseExpression()
+                    eat(')'.code)
+                } else if (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) { // numbers
+                    while (ch >= '0'.code && ch <= '9'.code || ch == '.'.code) nextChar()
+                    x = str.substring(startPos, pos).toDouble()
+                } else {
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+
+                if (eat('%'.code)) x /= 100.0 // percentage
+
+                return x
+            }
+        }.parse()
+    }
+}
